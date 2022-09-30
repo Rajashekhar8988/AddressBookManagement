2c9039e0739f314d178c49ae6fdcaa6d88b77581
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-30 13:24:40 +0530

    add : adding new contact details to the address book

--- AddressBook.java
+++ AddressBook.java
@@ -79,31 +79,50 @@ class ContactDetails {
 		    	return firstName+" "+lastName+" "+address+" "+emailId+" "+state+" "+phoneNumber+" "+zipCode;
 		    }
 }
-	class AddressBook {
-		public static void main(String[] args) {
-			LinkedList<ContactDetails> contact=new LinkedList<ContactDetails>();
-			System.out.println("Hey... Welcome to AddressBook");
-			Scanner sc=new Scanner(System.in);
-					System.out.println("Enter first name : ");
-					String firstName=sc.next();
-					System.out.println("Enter second name : ");
-					String secondName=sc.next();
-					System.out.println("Enter address name : ");
-					String address=sc.next();
-					System.out.println("Enter emailId name : ");
-					String emailId=sc.next();
-					System.out.println("Enter state name : ");
-					String state=sc.next();
-					System.out.println("Enter phoneNumber name : ");
-					String phoneNumber=sc.next();
-					System.out.println("Enter zipCode name : ");
-					String zipCode=sc.next();
-					
-				contact.add(new ContactDetails(firstName,secondName,address,emailId,state,phoneNumber,zipCode));
+class AddressBook {
+	public static void main(String[] args) {
+		LinkedList<ContactDetails> contact=new LinkedList<ContactDetails>();
+		Scanner sc=new Scanner(System.in);
+		int option;
+		do {
+			System.out.println("1.AddContact");
+			System.out.println("2.Display");
+			System.out.println("Enter your choice");
+			option=sc.nextInt();
+			switch(option) {
+			//add contact details into the address book
+			case 1:
+				System.out.println("Enter first name : ");
+				String firstName=sc.next();
+				System.out.println("Enter second name : ");
+				String secondName=sc.next();
+				System.out.println("Enter address name : ");
+				String address=sc.next();
+				System.out.println("Enter emailId name : ");
+				String emailId=sc.next();
+				System.out.println("Enter state name : ");
+				String state=sc.next();
+				System.out.println("Enter phoneNumber name : ");
+				String phoneNumber=sc.next();
+				System.out.println("Enter zipCode name : ");
+				String zipCode=sc.next();
+				
+			contact.add(new ContactDetails(firstName,secondName,address,emailId,state,phoneNumber,zipCode));
+			break;
+			//display the contact details present in address book
+			case 2:
+				System.out.println("-----------------------");
 				Iterator<ContactDetails> iterate=contact.iterator();
-				while(iterate.hasNext()) {
+				if(iterate.hasNext()) {
 					ContactDetails c=iterate.next();
-					System.out.println("Contact details available in address book : ");
+					System.out.println(c);
 				}
-		}
+				else {
+					System.out.println("There is no details, please add data contact details");
+				}
+				System.out.println("-----------------------");
+			break;
+			}
+		}while(option!=0);
 	}
+}
